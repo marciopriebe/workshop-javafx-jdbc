9151787408e5234d09895f2eb8accbea35bfef65
marciopriebe <m-priebe@hotmail.com>
2023-06-11 08:21:54 -0300

    MainView

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,18 @@
 module workshop_javafx_jdbc {
 	requires javafx.controls;
+    requires java.net.http;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;   
+    requires java.logging;
+    requires java.desktop;
+    
 	
+	
+	 
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.fxml;
+	
+	//exports gui to javafx.fxml;
 }
+
